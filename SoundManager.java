6286e325083b6f34ff7283bca64dc37201cd8818
Lukey26 <lukenobbee2003@gmail.com>
2025-04-25 13:32:12 -0400

    Audio

--- SoundManager.java
+++ SoundManager.java
@@ -1,71 +1,68 @@
-import javax.sound.sampled.AudioInputStream;		// for playing sound clips
+import javax.sound.sampled.AudioInputStream; // for playing sound clips
 import javax.sound.sampled.*;
 import java.io.*;
 
-import java.util.HashMap;				// for storing sound clips
+import java.util.HashMap; // for storing sound clips
 
-
-public class SoundManager {				// a Singleton class
+public class SoundManager { // a Singleton class
 	HashMap<String, Clip> clips;
 
-   	Clip hitClip = null;				// played when bat hits ball
-   	Clip appearClip = null;				// played when ball is re-generated
-   	Clip backgroundClip = null;			// played continuously after ball is created
+	Clip hitClip = null; // played when bat hits ball
+	Clip appearClip = null; // played when ball is re-generated
+	Clip backgroundClip = null; // played continuously after ball is created
 
-	private static SoundManager instance = null;	// keeps track of Singleton instance
+	private static SoundManager instance = null; // keeps track of Singleton instance
 
-	private SoundManager () {
+	private SoundManager() {
 		Clip clip;
 		clips = new HashMap<String, Clip>();
-		
-		//Clip clip = loadClip("sounds/background.wav");
-		//clips.put("background", clip);		// background theme sound
+
+		// Clip clip = loadClip("sounds/background.wav");
+		// clips.put("background", clip); // background theme sound
 
 		clip = loadClip("sounds/hitSound.wav");
-		clips.put("hit", clip);			// played when player's sprite collides 
-							//   with another sprice
+		clips.put("hit", clip); // played when player's sprite collides
+		// with another sprice
 
 		clip = loadClip("sounds/appearSound.wav");
-		clips.put("appear", clip);		// played when a special sprite 
-							//   makes an appearance
+		clips.put("appear", clip); // played when a special sprite
+		// makes an appearance
 
 		clip = loadClip("sounds/BirdSound.wav");
-		clips.put("birdSound", clip);		// played for bird-flying animation
+		clips.put("birdSound", clip); // played for bird-flying animation
+
+		clip = loadClip("sounds/shoot.wav");
+		clips.put("shoot", clip); // played when player shoots
 	}
 
-
-	public static SoundManager getInstance() {	// class method to get Singleton instance
+	public static SoundManager getInstance() { // class method to get Singleton instance
 		if (instance == null)
 			instance = new SoundManager();
-		
+
 		return instance;
-	}		
-
-
-	public Clip getClip (String title) {
-
-		return clips.get(title);		// gets a sound by supplying key
 	}
 
+	public Clip getClip(String title) {
 
-    	public Clip loadClip (String fileName) {	// gets clip from the specified file
- 		AudioInputStream audioIn;
+		return clips.get(title); // gets a sound by supplying key
+	}
+
+	public Clip loadClip(String fileName) { // gets clip from the specified file
+		AudioInputStream audioIn;
 		Clip clip = null;
 
 		try {
-    			File file = new File(fileName);
-    			audioIn = AudioSystem.getAudioInputStream(file.toURI().toURL()); 
-    			clip = AudioSystem.getClip();
-    			clip.open(audioIn);
+			File file = new File(fileName);
+			audioIn = AudioSystem.getAudioInputStream(file.toURI().toURL());
+			clip = AudioSystem.getClip();
+			clip.open(audioIn);
+		} catch (Exception e) {
+			System.out.println("Error opening sound files: " + e);
 		}
-		catch (Exception e) {
- 			System.out.println ("Error opening sound files: " + e);
-		}
-    		return clip;
-    	}
+		return clip;
+	}
 
-
-    	public void playSound(String title, Boolean looping) {
+	public void playSound(String title, Boolean looping) {
 		Clip clip = getClip(title);
 		if (clip != null) {
 			clip.setFramePosition(0);
@@ -74,14 +71,13 @@ public class SoundManager {				// a Singleton class
 			else
 				clip.start();
 		}
-    	}
+	}
 
-
-    	public void stopSound(String title) {
+	public void stopSound(String title) {
 		Clip clip = getClip(title);
 		if (clip != null) {
 			clip.stop();
 		}
-    	}
+	}
 
 }
